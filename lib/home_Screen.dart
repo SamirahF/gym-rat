39e17dcc271c9d4638a1c37195947fef1ff9682b
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-02-07 15:23:37 +0300

    added saperate pages for navbar

--- lib/home_Screen.dart
+++ lib/home_Screen.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:gym_rat/nav_screens.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  _HomeScreen createState() {
+    return _HomeScreen();
+  }
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  Widget activeScreen = const TimerScreen();
+  void switchScreen(index) {
+    if (index == 0) {
+      setState(() {
+        activeScreen = const TimerScreen();
+      });
+    }
+
+    if (index == 1) {
+      setState(() {
+        activeScreen = const LibraryScreen();
+      });
+    }
+
+    if (index == 2) {
+      setState(() {
+        activeScreen = const CalculatorScreen();
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 39, 39, 39),
-      body: const Center(
-        child: Text(
-          'Welcome to the Home Page!',
-          style: TextStyle(
-              fontSize: 24.0, color: Color.fromARGB(255, 199, 248, 86)),
-        ),
-      ),
+      body: activeScreen,
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: const Color.fromARGB(255, 199, 248, 86),
         color: const Color.fromARGB(255, 39, 39, 39),
@@ -36,7 +60,7 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
         onTap: (index) {
-          //Handle button tap
+          switchScreen(index);
         },
       ),
     );
