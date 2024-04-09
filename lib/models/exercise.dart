daf7ded1f4170ef843ec36a59740e80510592e8e
Samirah Alfouzan <samirahaaf2022@gmail.com>
2024-04-09 23:04:36 +0300

    create exercise model

---
+++ lib/models/exercise.dart
@@ -0,0 +1,10 @@
+class Exercise {
+  Exercise(
+      {required this.name,
+      required this.description,
+      required this.youtubeURL});
+
+  final String name;
+  final String description;
+  final String youtubeURL;
+}
